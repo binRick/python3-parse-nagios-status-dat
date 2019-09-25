90097ad9f1c413b52e6b8f7dfa3d831f0be83394
rblundell <none>
2019-09-25 12:21:51 +0000

    sync

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 python3 -m venv .venv
-rm -rf build dist test.spec
+rm -rf build dist test.spec parse-nagios-status
 source .venv/bin/activate
 pip install pip --upgrade
 pip install pyinstaller PyCrypto
@@ -12,3 +12,10 @@ pyinstaller \
     --key=8328274898256634 \
     --clean \
         parse-nagios-status.py
+
+
+
+mv dist/parse-nagios-status parse-nagios-status
+
+rm -rf build dist test.spec __pycache__
+~
