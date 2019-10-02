676d888f1a0584c7ff82369cba1f4965b5182f20
root <root@vpn299.vpnservice.company>
2019-10-02 17:36:48 -0400

    sync

--- cythonCompile.sh
+++ cythonCompile.sh
@@ -4,10 +4,12 @@ set -e
 python3 -m venv .venv-cython
 source .venv-cython/bin/activate
 pip install pip --upgrade
-pip install git+$(git config --get remote.origin.url)
+pip install git+ssh://git@github.com/binRick/python3-parse-nagios-status-dat.git@master
+
 pip install Cython
-rm -rf parseNagiosStatus.c parseNagiosStatus
+rm -rf parseNagiosStatus.c parseNagiosStatus parseNagiosStatus-cython
 cython -3 --embed -o parseNagiosStatus.c parseNagiosStatus.py
 gcc -Os -I /usr/include/python3.6m -o parseNagiosStatus parseNagiosStatus.c -lpython3.6m -lpthread -lm -lutil -ldl
-rm -rf parseNagiosStatus.c
+#rm -rf parseNagiosStatus.c
 mv parseNagiosStatus parseNagiosStatus-cython
+ls -al parseNagiosStatus-cython
